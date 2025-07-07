1f50961139c8e20d0188cb0ff113bf88f3d04577
suraj <surajkumarsahani1997@gmail.com>
2025-07-07 21:34:40 +0530

    added implicit return

--- src/components/FlowchartBuilder.tsx
+++ src/components/FlowchartBuilder.tsx
@@ -920,7 +920,8 @@ const selectedNodeDataForProperties = selectedNodeForProperties
                   </button>
                 )}
               </div>
-            )})}
+               );
+            })}
 
 
             {/* Render Presence Bubbles for other users */}
